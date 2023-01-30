ecc3d6ac9c1916152da9e5734ccf659485ace61d
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-30 16:25:14 +0200

    updated to vite to fix npm packaging issues

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ValidationForm } from '@aleksasdev/validation-form';
+
+function App() {
+
+   return (
+      <ValidationForm>
+         
+      </ValidationForm>
+   )
+}
+
+export default App
